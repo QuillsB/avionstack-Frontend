d64a94d61f033c681b214f262fae6879713255ea
Louis Beugnon <louis.beugnon@epitech.eu>
2021-03-13 11:49:04 +0100

    [Fix/Pagination] Pagination will now return to page 1 when new filters are applied.

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -16,6 +16,7 @@ export interface StateProps {
 
 interface State {
   actualPage: number;
+  actualAirline: string;
 }
 
 type Props = {} & DispatchProps & StateProps;
@@ -25,17 +26,19 @@ class Pagination extends Component<Props, State> {
     super(props);
     this.state = {
       actualPage: 1,
+      actualAirline: '',
     };
   }
 
   static getDerivedStateFromProps(nextProps: Props, prevState: State) {
-    const { paginationData: { offset, limit } } = nextProps;
-    const { actualPage } = prevState;
+    const { filterParams: { airline_name } } = nextProps;
+    const { actualAirline } = prevState;
 
-    if (offset && limit) {
-      if ((actualPage - 1) * limit !== offset) {
+    if (airline_name) {
+      if (airline_name !== actualAirline) {
         return {
-          actualPage: (offset / limit) + 1,
+          actualPage: 1,
+          actualAirline: airline_name,
         };
       }
       return null;
@@ -54,20 +57,32 @@ class Pagination extends Component<Props, State> {
     const { actualPage } = this.state;
     const { paginationData: { limit, total } } = this.props;
 
+    const lastPage = (total / limit) > 100 ? 100 : Math.round(total / limit);
+
     return (
       <div className="PaginationSelector">
         {actualPage > 1 &&
-          <div className="PageSelection" onClick={() => this.onSelectPage(actualPage - 1)}>
-            <p className="Selector">{actualPage - 1}</p>
-          </div>
+          <>
+            <div className="PageSelection" onClick={() => this.onSelectPage(1)}>
+              <p className="Selector">{"<<"}</p>
+            </div>
+            <div className="PageSelection" onClick={() => this.onSelectPage(actualPage - 1)}>
+              <p className="Selector">{actualPage - 1}</p>
+            </div>
+          </>
         }
         <div className="PageSelection PageSelected">
           <p className="Selector">{actualPage}</p>
         </div>
-        {(total / limit) > actualPage &&
-          <div className="PageSelection" onClick={() => this.onSelectPage(actualPage + 1)}>
-            <p className="Selector">{actualPage + 1}</p>
-          </div>
+        {lastPage > actualPage &&
+          <>
+            <div className="PageSelection" onClick={() => this.onSelectPage(actualPage + 1)}>
+              <p className="Selector">{actualPage + 1}</p>
+            </div>
+            <div className="PageSelection" onClick={() => this.onSelectPage(lastPage)}>
+              <p className="Selector">{">>"}</p>
+            </div>
+          </>
         }
       </div>
     );
